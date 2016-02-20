unixsurfer/haproxystats

commit b61ce6bcc97eef26b05f173ccad755bb38ce467a

Introduce pull-timeout
We don't want to wait indefinitely for tasks to be completed. It makes
no sense in a statistics collector which runs every X secs. We use
pull-timeout as the maximum time that it takes to:

- connect to all sockets
- send commands(show info and show stat)
- read data from the socket
- write data to the disk

haproxystats/pull.py
@@ -18,7 +18,7 @@ Options:
 """
 import os
 import asyncio
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, ALL_COMPLETED
 import sys
 import time
 import signal
@@ -129,6 +129,9 @@ def pull_stats(config, storage_dir, loop, executor):
     # absolute directory path which contains UNIX socket files.
     socket_dir = config.get('pull', 'socket-dir')
     timeout = config.getfloat('pull', 'timeout')
+    pull_timeout = config.getfloat('pull', 'pull-timeout')
+    if int(pull_timeout) == 0:
+        pull_timeout = None
 
     while True:
         socket_files = [f for f in glob.glob(socket_dir + '/*')
@@ -144,9 +147,12 @@ def pull_stats(config, storage_dir, loop, executor):
                   for socket_file in socket_files
                   for cmd in CMDS]
     # Launch all connections.
-    status = yield from asyncio.gather(*coroutines)
+    done, pending = yield from asyncio.wait(coroutines,
+                                            timeout=pull_timeout,
+                                            return_when=ALL_COMPLETED)
+    log.debug('task report, done:%s pending:%s tasks', len(done), len(pending))
 
-    return len(set(status)) == 1 and True in set(status)
+    return not pending  # only when all tasks are finished we claim success
 
 
 def supervisor(loop, config, executor):

haproxystats.conf
@@ -13,6 +13,7 @@ socket-dir      = /run/haproxy
 retries         = 1
 timeout         = 1
 interval        = 1
+pull-timeout    = 0.5
 pull-interval   = 10
 dst-dir         = ${paths:base-dir}/incoming
 tmp-dst-dir     = ${paths:base-dir}/incoming.tmp

haproxystats/__init__.py
@@ -25,6 +25,7 @@ DEFAULT_OPTIONS = {
         'retries': '1',
         'timeout': '1',
         'interval': '1',
+        'pull-timeout': '0.5',
         'pull-interval': '10',
         'dst-dir': '/var/lib/haproxystats/incoming',
         'tmp-dst-dir': '/var/lib/haproxystats/incoming.tmp',

commit 48dec8e744d1508e099f5660d3e236e4f60dde09

Fix a regression introduced by b61ce6bcc
In b61ce6bcc we introduced a pull-timeout which sets a timeout for
pulling data(info and stats) from all UNIX sockets.  But, we never
checked if they actual pull of data finished successfully.
The coroutines returned False as they received exception when
trying to connect to UNIX sockets, but their status wasn't checked.
As a result supervisor() was assuming everything went fine and moved the
directory with no files to final destination. The process program didn't
handle this situation very well as workers got exceptions from Pandas as
we passed empty data and crashed, actually workers became orphan
processes:-(. Process program is now more robust and avoids to pass
empty data to Pandas.

With this commit we check the result of each finished task and we claim
success only when all tasks are finished successfully.

haproxystats/pull.py
@@ -126,6 +126,7 @@ def pull_stats(config, storage_dir, loop, executor):
         True if statistics from *all* UNIX sockets are fetched False otherwise.
     """
     # absolute directory path which contains UNIX socket files.
+    results = []  # stores the result of finished tasks
     socket_dir = config.get('pull', 'socket-dir')
     timeout = config.getfloat('pull', 'timeout')
     pull_timeout = config.getfloat('pull', 'pull-timeout')
@@ -149,9 +150,17 @@ def pull_stats(config, storage_dir, loop, executor):
     done, pending = yield from asyncio.wait(coroutines,
                                             timeout=pull_timeout,
                                             return_when=ALL_COMPLETED)
-    log.debug('task report, done:%s pending:%s tasks', len(done), len(pending))
+    for task in done:
+        log.debug('task status: %s', task)
+        results.append(task.result())
+    log.debug('task report, done:%s pending:%s succeed:%s failed:%s',
+              len(done),
+              len(pending),
+              results.count(True),
+              results.count(False))
 
-    return not pending  # only when all tasks are finished we claim success
+    # only when all tasks are finished successfully we claim success
+    return not pending and len(set(results)) == 1 and True in set(results)
 
 
 def supervisor(loop, config, executor):