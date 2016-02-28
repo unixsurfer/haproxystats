unixsurfer/haproxystats

commit 5d1003caeec43e36a39f04de4f43494164723a97

Use float rather int for measuring wall clock

haproxystats/pull.py
@@ -217,7 +217,7 @@ def supervisor(loop, config, executor):
             log.warning('queue reached max size of %s, pulling statistics is '
                         'suspended', len(queue))
             time.sleep(config.getint('pull', 'pull-interval'))
-        start_time = int(time.time())
+        start_time = time.time()
         # HAProxy statistics are stored in a directory and we use retrieval
         # time(seconds since the Epoch) as a name of the directory.
         # We first store them in a temporary place until we receive statistics

commit eeebfa18ebb67dc4a7dfea3d7ad8eaae82326d44

Fix regression introduced by 5d1003ca
5d1003ca changed start time from int to float as we needed it for
calculating wallclock time, but the directory name shouldn't change.
Convert epoch from float to int as we don't want to change the directory
name schema.

haproxystats/pull.py
@@ -222,7 +222,7 @@ def supervisor(loop, config, executor):
         # time(seconds since the Epoch) as a name of the directory.
         # We first store them in a temporary place until we receive statistics
         # from all UNIX sockets.
-        storage_dir = os.path.join(tmp_dst_dir, str(start_time))
+        storage_dir = os.path.join(tmp_dst_dir, str(int(start_time)))
 
         # If our storage directory can't be created we can't do much, thus
         # abort main program.