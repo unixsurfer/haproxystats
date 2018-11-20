95f2988ed290992da881a562723845cd5a32c921
Pavlos Parissis <pavlos.parissis@booking.com>
2018-11-20 16:33:54 +0100

    RELEASE 0.4.1 version

--- haproxystats/__init__.py
+++ haproxystats/__init__.py
@@ -5,7 +5,7 @@
 __title__ = 'haproxystats'
 __author__ = 'Pavlos Parissis'
 __license__ = 'Apache 2.0'
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 __copyright__ = 'Copyright 2016 Pavlos Parissis <pavlos.parissis@gmail.com'
 
 DEFAULT_OPTIONS = {
